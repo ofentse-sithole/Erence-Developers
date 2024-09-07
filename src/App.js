dd611dfbc33e76fd59f10e7a79b7e6e3e597b655
Ofentse Sithole <ST10104487@rcconnect.edu.za>
2024-09-07 21:02:15 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar';
-import Home from './components/pages/home'
+import Home from './components/pages/home';
 import Services from './components/pages/Services';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 
-/*adding this because of react-dom*/ 
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+/*adding this because of react-dom*/
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
 
 function App() {
   return (
-   <>
-   <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/home" element={<Home/>}/>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Default route */}
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
-      </Routes>
+        </Routes>
       </Router>
     </>
   );
